230553593a3b6329264fa1c45b53edd62d7834c8
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2020-10-06 18:26:50 +0900

    add qhulleigen

---
+++ 3rdparty/qhulleigen/include/qhulleigen/qhulleigen.h
@@ -0,0 +1,16 @@
+#ifndef QHULLEIGEN_H
+#define QHULLEIGEN_H
+
+extern "C" {
+#include <qhull/qhull_a.h>
+}
+
+#include <Eigen/Eigen>
+
+namespace qhulleigen{
+  // In = [v1 v2 v3 v4 ..]
+  bool convexhull(const Eigen::MatrixXd& In, Eigen::MatrixXd& Out, std::vector<std::vector<int> >& Face);
+};
+
+
+#endif
---
+++ 3rdparty/qhulleigen/src/qhulleigen.cpp
@@ -0,0 +1,66 @@
+#include <qhulleigen/qhulleigen.h>
+
+namespace qhulleigen{
+  bool convexhull(const Eigen::MatrixXd& In, Eigen::MatrixXd& Out, std::vector<std::vector<int> >& Face){
+    // convex hull by qhull.
+    int numVertices = In.cols();
+    int dim = In.rows();
+    double points[numVertices*dim];
+    for (int i=0; i<numVertices; i++){
+      for(size_t j=0;j<dim;j++){
+        points[i*dim+j] = In(j,i);
+      }
+    }
+    char flags[250];
+    boolT ismalloc = False;
+    sprintf(flags,"qhull Fx");
+    if (qh_new_qhull (dim,numVertices,points,ismalloc,flags,NULL,stderr)) return false;
+
+    qh_triangulate();
+    qh_vertexneighbors();
+
+    std::vector<Eigen::VectorXd> hull;
+    int index[numVertices];
+    int vertexIndex = 0;
+    vertexT *vertex;
+    FORALLvertices {
+      int p = qh_pointid(vertex->point);
+      index[p] = vertexIndex;//なぜか分からないがpの値が数百あるので，多分ここでメモリ確保違反をしている
+      hull.push_back(In.col(p));
+      vertexIndex++;
+    }
+    Out.resize(dim,hull.size());
+    for(size_t i=0;i<hull.size();i++){
+      Out.col(i) = hull[i];
+    }
+
+    Face.clear();
+
+    facetT *facet;
+    int num = qh num_facets;
+    int triangleIndex = 0;
+    FORALLfacets {
+      int j = 0;
+      std::vector<int> p;
+      p.reserve(dim);
+      setT *vertices = facet->vertices;
+      //setT *vertices = qh_facet3vertex (facet);
+      vertexT **vertexp;
+      FOREACHvertexreverse12_ (vertices) {
+        if (j<dim) {
+          p.push_back(index[qh_pointid(vertex->point)]);
+        } else {
+          fprintf(stderr, "extra vertex %d\n",j);
+        }
+        j++;
+      }
+      Face.push_back(p);
+    }
+
+    qh_freeqhull(!qh_ALL);
+    int curlong, totlong;
+    qh_memfreeshort (&curlong, &totlong);
+
+    return true;
+  }
+}
