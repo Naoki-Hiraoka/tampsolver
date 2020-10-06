8e39779649200e293516e5d4515b6049d40906b5
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2020-10-06 15:03:24 +0900

    fix cddeigen

--- 3rdparty/cddeigen/include/cddeigen/cddeigen.h
+++ 3rdparty/cddeigen/include/cddeigen/cddeigen.h
@@ -13,14 +13,14 @@ namespace cddeigen{
     OUTPUT:
       x = V y + R_nonneg z + R_free w (sum y = 1, y >= 0, z >= 0)
   */
-  bool HtoV (const Eigen::MatrixXd& A_eq,
-             const Eigen::VectorXd& b_eq,
-             const Eigen::MatrixXd& A_ineq,
-             const Eigen::VectorXd& b_ineq,
-             Eigen::MatrixXd& V,
-             Eigen::MatrixXd& R_nonneg,
-             Eigen::MatrixXd& R_free,
-             bool verbose=false);
+  bool HtoVgmp (const Eigen::MatrixXd& A_eq,
+                const Eigen::VectorXd& b_eq,
+                const Eigen::MatrixXd& A_ineq,
+                const Eigen::VectorXd& b_ineq,
+                Eigen::MatrixXd& V,
+                Eigen::MatrixXd& R_nonneg,
+                Eigen::MatrixXd& R_free,
+                bool verbose=false);
 
   /*
     INPUT:
@@ -29,14 +29,14 @@ namespace cddeigen{
       A_eq   x + b_eq    = 0
       A_ineq x + b_ineq >= 0
   */
-  bool VtoH (const Eigen::MatrixXd& V,
-             const Eigen::MatrixXd& R_nonneg,
-             const Eigen::MatrixXd& R_free,
-             Eigen::MatrixXd& A_eq,
-             Eigen::VectorXd& b_eq,
-             Eigen::MatrixXd& A_ineq,
-             Eigen::VectorXd& b_ineq,
-             bool verbose=false);
+  bool VtoHgmp (const Eigen::MatrixXd& V,
+                const Eigen::MatrixXd& R_nonneg,
+                const Eigen::MatrixXd& R_free,
+                Eigen::MatrixXd& A_eq,
+                Eigen::VectorXd& b_eq,
+                Eigen::MatrixXd& A_ineq,
+                Eigen::VectorXd& b_ineq,
+                bool verbose=false);
 };
 
 #endif
--- 3rdparty/cddeigen/src/cddeigen.cpp
+++ 3rdparty/cddeigen/src/cddeigen.cpp
@@ -4,14 +4,17 @@
 namespace cddeigen{
   static bool initialized=false;
 
-  bool HtoV (const Eigen::MatrixXd& A_eq,
-             const Eigen::VectorXd& b_eq,
-             const Eigen::MatrixXd& A_ineq,
-             const Eigen::VectorXd& b_ineq,
-             Eigen::MatrixXd& V,
-             Eigen::MatrixXd& R_nonneg,
-             Eigen::MatrixXd& R_free,
-             bool verbose){
+  //ddをddfに変えるとgmpを使わなくなる
+  //が，今はcddをリンクしているのでgmpを使っていない
+
+  bool HtoVgmp (const Eigen::MatrixXd& A_eq,
+                const Eigen::VectorXd& b_eq,
+                const Eigen::MatrixXd& A_ineq,
+                const Eigen::VectorXd& b_ineq,
+                Eigen::MatrixXd& V,
+                Eigen::MatrixXd& R_nonneg,
+                Eigen::MatrixXd& R_free,
+                bool verbose){
     if(!initialized) dd_set_global_constants();
 
     if( (A_eq.rows() != b_eq.rows()) || (A_ineq.rows() != b_ineq.rows()) || (A_eq.cols() != A_ineq.cols())) {
@@ -20,100 +23,114 @@ namespace cddeigen{
     }
 
     // setup A
-    ddf_MatrixPtr A = ddf_CreateMatrix(A_eq.rows() + A_ineq.rows(),A_eq.cols()+1);
+    dd_MatrixPtr A = dd_CreateMatrix(A_eq.rows() + A_ineq.rows(),A_eq.cols()+1);
     for (size_t i = 0; i < A_eq.rows(); i++){
       set_addelem(A->linset,i+1);
 
-      ddf_set_d(A->matrix[i][0], b_eq[i]);
+      dd_set_d(A->matrix[i][0], b_eq[i]);
       for (size_t j = 0; j < A_eq.cols(); j++){
-        ddf_set_d(A->matrix[i][j+1], A_eq(i,j));
+        dd_set_d(A->matrix[i][j+1], A_eq(i,j));
       }
     }
     for (size_t i = 0; i < A_ineq.rows(); i++){
-      ddf_set_d(A->matrix[A_eq.rows()+i][0], b_ineq[i]);
+      dd_set_d(A->matrix[A_eq.rows()+i][0], b_ineq[i]);
       for (size_t j = 0; j < A_ineq.cols(); j++){
-        ddf_set_d(A->matrix[A_eq.rows()+i][j+1], A_ineq(i,j));
+        dd_set_d(A->matrix[A_eq.rows()+i][j+1], A_ineq(i,j));
       }
     }
-    A->representation=ddf_Inequality;
+    A->representation=dd_Inequality;
 
-    ddf_rowindex newpos;
-    ddf_rowset impl_linset,redset;
-    ddf_ErrorType errf;
-    ddf_MatrixCanonicalize(&A, &impl_linset, &redset, &newpos, &errf);
+    if (verbose){
+      printf("\ninput H-representation:\n");
+      dd_WriteMatrix(stderr,A);  printf("\n");
+    }
+
+    dd_rowindex newpos;
+    dd_rowset impl_linset,redset;
+    dd_ErrorType errf;
+    dd_MatrixCanonicalize(&A, &impl_linset, &redset, &newpos, &errf);
+
+    if (verbose){
+      printf("\nafter canonicalize:\n");
+      dd_WriteMatrix(stderr,A);  printf("\n");
+    }
 
     // compute the second (generator) representation
-    ddf_ErrorType err;
-    ddf_PolyhedraPtr poly=ddf_DDMatrix2Poly(A, &err);
+    dd_ErrorType err;
+    dd_PolyhedraPtr poly=dd_DDMatrix2Poly(A, &err);
 
-    if (err==ddf_NoError){
-      ddf_MatrixPtr G = ddf_CopyGenerators(poly);
+    if (err==dd_NoError){
+      dd_MatrixPtr G = dd_CopyGenerators(poly);
+
+      if (verbose){
+        printf("\noutput V-representation:\n");
+        dd_WriteMatrix(stdout,G);
+      }
 
       // get matrix size
       int V_cols = 0;
       int R_nonneg_cols = 0;
       int R_free_cols = 0;
       for (size_t i = 0; i < G->rowsize; i++){
-        if (ddf_get_d(G->matrix[i][0]) ==0 ){
+        if (dd_get_d(G->matrix[i][0]) ==0 ){
           if (set_member(i+1,G->linset)) R_free_cols++;
           else R_nonneg_cols++;
         }else V_cols++;
       }
 
       // create return matrix
-      V = Eigen::MatrixXd(A_eq.cols(),V_cols);
-      R_nonneg = Eigen::MatrixXd(A_eq.cols(),R_nonneg_cols);
-      R_free = Eigen::MatrixXd(A_eq.cols(),R_free_cols);
+      V.resize(A_eq.cols(),V_cols);
+      R_nonneg.resize(A_eq.cols(),R_nonneg_cols);
+      R_free.resize(A_eq.cols(),R_free_cols);
       int V_idx = 0;
       int R_nonneg_idx = 0;
       int R_free_idx = 0;
       for (size_t i = 0; i < G->rowsize; i++){
-        if (ddf_get_d(G->matrix[i][0])==0){
+        if (dd_get_d(G->matrix[i][0])==0){
           if (set_member(i+1,G->linset)){
             for (size_t j = 0; j < A_eq.cols(); j++){
-              R_free(j,R_free_idx) = ddf_get_d(G->matrix[i][1+j]);
+              R_free(j,R_free_idx) = dd_get_d(G->matrix[i][1+j]);
             }
             R_free_idx++;
           }else{
             for (size_t j = 0; j < A_eq.rows(); j++){
-              R_nonneg(j,R_nonneg_idx) = ddf_get_d(G->matrix[i][1+j]);
+              R_nonneg(j,R_nonneg_idx) = dd_get_d(G->matrix[i][1+j]);
             }
             R_nonneg_idx++;
           }
         }else{
           for (size_t j = 0; j < A_eq.rows(); j++){
-            V(j,V_idx) = ddf_get_d(G->matrix[i][1+j]);
+            V(j,V_idx) = dd_get_d(G->matrix[i][1+j]);
           }
           V_idx++;
         }
       }
 
       // free
-      ddf_FreeMatrix(A);
-      ddf_FreeMatrix(G);
-      ddf_FreePolyhedra(poly);
-
+      dd_FreeMatrix(A);
+      dd_FreeMatrix(G);
+      dd_FreePolyhedra(poly);
       return true;
     }else{
       if (verbose){
-        ddf_WriteErrorMessages(stdout,err);
+        dd_WriteErrorMessages(stdout,err);
       }
 
-      ddf_FreeMatrix(A);
-      ddf_FreePolyhedra(poly);
+      dd_FreeMatrix(A);
+      dd_FreePolyhedra(poly);
 
       return false;
     }
   }
 
-  bool VtoH (const Eigen::MatrixXd& V,
-             const Eigen::MatrixXd& R_nonneg,
-             const Eigen::MatrixXd& R_free,
-             Eigen::MatrixXd& A_eq,
-             Eigen::VectorXd& b_eq,
-             Eigen::MatrixXd& A_ineq,
-             Eigen::VectorXd& b_ineq,
-             bool verbose){
+  bool VtoHgmp (const Eigen::MatrixXd& V,
+                const Eigen::MatrixXd& R_nonneg,
+                const Eigen::MatrixXd& R_free,
+                Eigen::MatrixXd& A_eq,
+                Eigen::VectorXd& b_eq,
+                Eigen::MatrixXd& A_ineq,
+                Eigen::VectorXd& b_ineq,
+                bool verbose){
     if(!initialized) dd_set_global_constants();
 
     if( (V.rows() != R_nonneg.rows()) || (R_nonneg.rows() != R_free.rows()) ) {
@@ -122,39 +139,54 @@ namespace cddeigen{
     }
 
     // setup G
-    ddf_MatrixPtr G=ddf_CreateMatrix(V.cols()+R_nonneg.cols()+R_free.cols(),V.rows()+1);
+    dd_MatrixPtr G=dd_CreateMatrix(V.cols()+R_nonneg.cols()+R_free.cols(),V.rows()+1);
     for (size_t i = 0; i < V.cols(); i++){
-      ddf_set_si(G->matrix[i][0],1);
+      dd_set_si(G->matrix[i][0],1);
       for (size_t j = 0; j < V.rows(); j++){
-        ddf_set_d(G->matrix[i][j+1],V(j,i));
+        dd_set_d(G->matrix[i][j+1],V(j,i));
       }
     }
     for (size_t i = 0; i < R_nonneg.cols(); i++){
-      ddf_set_si(G->matrix[V.cols()+i][0],0);
+      dd_set_si(G->matrix[V.cols()+i][0],0);
       for (size_t j = 0; j < V.rows(); j++){
-        ddf_set_d(G->matrix[V.cols()+i][j+1],R_nonneg(j,i));
+        dd_set_d(G->matrix[V.cols()+i][j+1],R_nonneg(j,i));
       }
     }
     for (size_t i = 0; i < R_free.cols(); i++){
       set_addelem(G->linset,V.cols()+R_nonneg.cols()+i+1);
-      ddf_set_si(G->matrix[V.cols()+R_nonneg.cols()+i][0],0);
+      dd_set_si(G->matrix[V.cols()+R_nonneg.cols()+i][0],0);
       for (size_t j = 0; j < V.rows(); j++){
-        ddf_set_d(G->matrix[V.cols()+R_nonneg.cols()+i][j+1],R_free(j,i));
+        dd_set_d(G->matrix[V.cols()+R_nonneg.cols()+i][j+1],R_free(j,i));
       }
     }
-    G->representation=ddf_Generator;
+    G->representation=dd_Generator;
 
-    ddf_rowindex newpos;
-    ddf_rowset impl_linset,redset;
-    ddf_ErrorType errf;
-    ddf_MatrixCanonicalize(&G, &impl_linset, &redset, &newpos, &errf);
+    if (verbose){
+      printf("\ninput V-representation:\n");
+      dd_WriteMatrix(stderr,G);  printf("\n");
+    }
+
+    dd_rowindex newpos;
+    dd_rowset impl_linset,redset;
+    dd_ErrorType errf;
+    dd_MatrixCanonicalize(&G, &impl_linset, &redset, &newpos, &errf);
+
+    if (verbose){
+      printf("\nafter canonicalize:\n");
+      dd_WriteMatrix(stderr,G);  printf("\n");
+    }
 
     // compute the second (generator) representation
-    ddf_ErrorType err;
-    ddf_PolyhedraPtr poly=ddf_DDMatrix2Poly(G, &err);
+    dd_ErrorType err;
+    dd_PolyhedraPtr poly=dd_DDMatrix2Poly(G, &err);
 
-    if (err==ddf_NoError){
-      ddf_MatrixPtr A = ddf_CopyInequalities(poly);
+    if (err==dd_NoError){
+      dd_MatrixPtr A = dd_CopyInequalities(poly);
+
+      if (verbose){
+        printf("\noutput H-representation:\n");
+        dd_WriteMatrix(stdout,A);
+      }
 
       // get matrix size
       int A_eq_rows = 0;
@@ -165,41 +197,41 @@ namespace cddeigen{
       }
 
       // create return matrix
-      A_eq = Eigen::MatrixXd(A_eq_rows,V.rows());
-      b_eq = Eigen::VectorXd(A_eq_rows);
-      A_ineq = Eigen::MatrixXd(A_ineq_rows,V.rows());
-      b_ineq = Eigen::VectorXd(A_ineq_rows);
+      A_eq.resize(A_eq_rows,V.rows());
+      b_eq.resize(A_eq_rows);
+      A_ineq.resize(A_ineq_rows,V.rows());
+      b_ineq.resize(A_ineq_rows);
       int A_eq_idx = 0;
       int A_ineq_idx = 0;
       for (size_t i = 0; i < A->rowsize; i++){
         if (set_member(i+1,A->linset)){
           for (size_t j = 0; j < V.rows(); j++){
-            A_eq(A_eq_idx,j) = ddf_get_d(A->matrix[i][1+j]);
+            A_eq(A_eq_idx,j) = dd_get_d(A->matrix[i][1+j]);
           }
-          b_eq[A_eq_idx] = ddf_get_d(A->matrix[i][0]);
+          b_eq[A_eq_idx] = dd_get_d(A->matrix[i][0]);
           A_eq_idx++;
         }else{
           for (size_t j = 0; j < V.rows(); j++){
-            A_ineq(A_ineq_idx,j) = ddf_get_d(A->matrix[i][1+j]);
+            A_ineq(A_ineq_idx,j) = dd_get_d(A->matrix[i][1+j]);
           }
-          b_ineq[A_ineq_idx] = ddf_get_d(A->matrix[i][0]);
+          b_ineq[A_ineq_idx] = dd_get_d(A->matrix[i][0]);
           A_ineq_idx++;
         }
       }
 
       // free
-      ddf_FreeMatrix(A);
-      ddf_FreeMatrix(G);
-      ddf_FreePolyhedra(poly);
+      dd_FreeMatrix(A);
+      dd_FreeMatrix(G);
+      dd_FreePolyhedra(poly);
 
       return true;
     }else{
       if (verbose){
-        ddf_WriteErrorMessages(stdout,err);
+        dd_WriteErrorMessages(stdout,err);
       }
 
-      ddf_FreeMatrix(G);
-      ddf_FreePolyhedra(poly);
+      dd_FreeMatrix(G);
+      dd_FreePolyhedra(poly);
 
       return false;
     }
