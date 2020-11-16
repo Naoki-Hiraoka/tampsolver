5314c0cc70d62a12dbcb87a63698e232862d5124
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2020-11-16 14:57:04 +0900

    add JointStateFilter

--- 3rdparty/iir_filter/include/iir_filter/Filter.h
+++ 3rdparty/iir_filter/include/iir_filter/Filter.h
@@ -1,24 +1,30 @@
 #ifndef IIR_FILTER_FILTER_H
 #define IIR_FILTER_FILTER_H
-#include <IIRFilter.h>
 
-template <class T> class FirstOrderLowPassFilter2
-{
-private:
+#include <IIRFilter.cpp>
+
+namespace iir_filter{
+  using IIRFilter=IIRFilter;
+  template <class T>
+  using FirstOrderLowPassFilter=FirstOrderLowPassFilter<T>;
+
+  template <class T> class FirstOrderLowPassFilter2
+  {
+  private:
     T prev_value;
     double cutoff_freq, dt, const_param;
-public:
+  public:
     FirstOrderLowPassFilter2 (const double _cutoff_freq, const double _dt, const T init_value) : prev_value(init_value), dt(_dt)
     {
-        setCutOffFreq(_cutoff_freq);
+      setCutOffFreq(_cutoff_freq);
     };
     ~FirstOrderLowPassFilter2()
     {
     };
     T passFilter (const T& value)
     {
-        prev_value = 1.0/(1+const_param) * prev_value + const_param/(1+const_param) * value;
-        return prev_value;
+      prev_value = 1.0/(1+const_param) * prev_value + const_param/(1+const_param) * value;
+      return prev_value;
     };
     T passFilter (const T& value, const double _dt)
     {
@@ -26,18 +32,19 @@ public:
         dt = _dt;
         setCutOffFreq(cutoff_freq);
       }
-        prev_value = 1.0/(1+const_param) * prev_value + const_param/(1+const_param) * value;
-        return prev_value;
+      prev_value = 1.0/(1+const_param) * prev_value + const_param/(1+const_param) * value;
+      return prev_value;
     };
     void reset (const T& value) { prev_value = value; };
     void setCutOffFreq (const double f)
     {
-        cutoff_freq = f;
-        const_param = 2 * M_PI * cutoff_freq * dt;
+      cutoff_freq = f;
+      const_param = 2 * M_PI * cutoff_freq * dt;
     };
     double getCutOffFreq () const { return cutoff_freq; };
     T getCurrentValue () const { return prev_value; };
-};
+  };
 
+};
 
 #endif
---
+++ multicontact_controller/src/JointStatesFilter/JointStatesFilter.cpp
@@ -0,0 +1,143 @@
+#include <iir_filter/Filter.h>
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <functional>
+
+class JointStateFilter {
+public:
+  JointStateFilter(const ros::Time& stamp, const std::string& name, const double& init_position, const double& init_velocity, const double& init_effort, const double& position_cutoff_freq, const double& velocity_cutoff_freq, const double& effort_cutoff_freq):
+    name_(name),
+    stamp_(stamp),
+    position_filtered_(init_position),
+    velocity_filtered_(init_velocity),
+    effort_filtered_(init_effort),
+    position_filter_(position_cutoff_freq,0.1,init_position),
+    velocity_filter_(velocity_cutoff_freq,0.1,init_velocity),
+    effort_filter_(effort_cutoff_freq,0.1,init_effort)
+  {
+  }
+  void passFilter(const ros::Time& stamp, const double& position, const double& velocity, const double& effort) {
+    double dt = (stamp - stamp_).toSec();
+    if(dt<=0) return;
+
+    position_filtered_ = position_filter_.passFilter(position,dt);
+    velocity_filtered_ = velocity_filter_.passFilter(velocity,dt);
+    effort_filtered_ = effort_filter_.passFilter(effort,dt);
+
+    stamp_ = stamp;
+  }
+
+  std::string& name() { return name_; }
+  std::string name() const { return name_; }
+  double& position() { return position_filtered_; }
+  double position() const {return position_filtered_; }
+  double& velocity() {return velocity_filtered_; }
+  double velocity() const {return velocity_filtered_; }
+  double& effort() {return effort_filtered_; }
+  double effort() const {return effort_filtered_; }
+private:
+  std::string name_;
+  ros::Time stamp_;
+  double position_filtered_;
+  double velocity_filtered_;
+  double effort_filtered_;
+  iir_filter::FirstOrderLowPassFilter2<double> position_filter_;
+  iir_filter::FirstOrderLowPassFilter2<double> velocity_filter_;
+  iir_filter::FirstOrderLowPassFilter2<double> effort_filter_;
+};
+
+
+void jointStateCallback(const sensor_msgs::JointState::ConstPtr& msg, std::map<std::string, std::shared_ptr<JointStateFilter> >& jointStateMap, const double& position_cutoff_freq, const double& velocity_cutoff_freq, const double& effort_cutoff_freq) {
+  const ros::Time stamp = msg->header.stamp;
+  for(size_t i=0;i<msg->name.size();i++){
+    const std::string& name = msg->name[i];
+    double position = msg->position.size()==msg->name.size() ? msg->position[i] : 0;
+    double velocity = msg->velocity.size()==msg->name.size() ? msg->velocity[i] : 0;
+    double effort = msg->effort.size()==msg->name.size() ? msg->effort[i] : 0;
+    if(jointStateMap.find(name) == jointStateMap.end()){
+      jointStateMap[name] = std::make_shared<JointStateFilter>(stamp,
+                                                               name,
+                                                               position,
+                                                               velocity,
+                                                               effort,
+                                                               position_cutoff_freq,
+                                                               velocity_cutoff_freq,
+                                                               effort_cutoff_freq);
+    }else{
+      jointStateMap[name]->passFilter(stamp,
+                                      position,
+                                      velocity,
+                                      effort);
+    }
+  }
+}
+
+
+int main(int argc, char** argv){
+  ros::init(argc,argv,"joint_states_filter");
+  ros::NodeHandle n;
+  ros::NodeHandle nl("~");
+
+  double position_cutoff_freq;
+  double velocity_cutoff_freq;
+  double effort_cutoff_freq;
+  nl.param("position_cutoff_freq", position_cutoff_freq, 1.0); // 1 hz
+  nl.param("velocity_cutoff_freq", velocity_cutoff_freq, 1.0); // 1 hz
+  nl.param("effort_cutoff_freq", effort_cutoff_freq, 1.0); // 1 hz
+
+  std::map<std::string, std::shared_ptr<JointStateFilter> > jointStateMap;
+  ros::Subscriber jointStateSub =
+    n.subscribe<sensor_msgs::JointState>("joint_states",
+                                         1000, // 一部しか含まないjoint_statesにも対応するため、バッファは1(最新のみ)では不可
+                                         [&](const sensor_msgs::JointState::ConstPtr& msg){
+                                           const ros::Time stamp = msg->header.stamp;
+                                           for(size_t i=0;i<msg->name.size();i++){
+                                             const std::string& name = msg->name[i];
+                                             double position = msg->position.size()==msg->name.size() ? msg->position[i] : 0;
+                                             double velocity = msg->velocity.size()==msg->name.size() ? msg->velocity[i] : 0;
+                                             double effort = msg->effort.size()==msg->name.size() ? msg->effort[i] : 0;
+                                             if(jointStateMap.find(name) == jointStateMap.end()){
+                                               jointStateMap[name] = std::make_shared<JointStateFilter>(stamp,
+                                                                                                        name,
+                                                                                                        position,
+                                                                                                        velocity,
+                                                                                                        effort,
+                                                                                                        position_cutoff_freq,
+                                                                                                        velocity_cutoff_freq,
+                                                                                                        effort_cutoff_freq);
+                                             }else{
+                                               jointStateMap[name]->passFilter(stamp,
+                                                                               position,
+                                                                               velocity,
+                                                                               effort);
+                                             }
+                                           }
+                                         });
+
+  ros::Publisher jointStateFilteredPub = n.advertise<sensor_msgs::JointState>("joint_states_filtered", 1000);
+
+  int rate;
+  nl.param("rate", rate, 250); // 250 hz
+  ros::Rate r(rate);
+
+  unsigned int seq = 0;
+  while (ros::ok()) {
+    // spin
+    ros::spinOnce();
+
+    sensor_msgs::JointState msg;
+    msg.header.stamp = ros::Time::now();
+    msg.header.seq = seq;
+    for(std::map<std::string, std::shared_ptr<JointStateFilter> >::iterator it=jointStateMap.begin();it!=jointStateMap.end();it++){
+      msg.name.push_back(it->second->name());
+      msg.position.push_back(it->second->position());
+      msg.velocity.push_back(it->second->velocity());
+      msg.effort.push_back(it->second->effort());
+    }
+    jointStateFilteredPub.publish(msg);
+    seq++;
+    r.sleep();
+  }
+
+  return 0;
+}
