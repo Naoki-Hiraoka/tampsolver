2aca43377ceb3bc44ea7f54aa65c7fa7ef742438
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-01-03 12:26:43 +0900

    add static_equilibuim_test

---
+++ 3rdparty/static_equilibuim_test/include/static_equilibuim_test/StaticEquilibuimTest.h
@@ -0,0 +1,26 @@
+#ifndef STATIC_EQUILIBUIM_TEST_H
+#define STATIC_EQUILIBUIM_TEST_H
+
+#include <Eigen/Eigen>
+
+namespace static_equilibuim_test{
+  // Ax = b
+  // dl <= Cx <= du
+  // x の 0,1番目の要素を投影
+  // 返り値
+  // l_out <= M_out x[0:1] <= u_out
+  bool calcProjection(const Eigen::SparseMatrix<double,Eigen::RowMajor>& A,
+                      const Eigen::VectorXd& b,
+                      const Eigen::SparseMatrix<double,Eigen::RowMajor>& C,
+                      const Eigen::VectorXd& dl,
+                      const Eigen::VectorXd& du,
+                      Eigen::SparseMatrix<double,Eigen::RowMajor>& M_out,
+                      Eigen::VectorXd& l_out,
+                      Eigen::VectorXd& u_out,
+                      int debuglevel = 0,
+                      double eps = 0.05,
+                      size_t maxiter = 30
+                      );
+}
+
+#endif
